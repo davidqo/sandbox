5686cbd8d1671b6e566dc59888f9c7c16929514c
Давиденко Дмитрий Вячеславович <dmitry.davidenko@eltex.loc>
2016-12-02 17:01:43 +0600

    Заглушка сервера на ковбое. Запускается make run. Порт 18080

---
+++ game/src/game_app.erl
@@ -0,0 +1,29 @@
+-module(game_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/", index_handler, []},
+            {'_', notfound_handler, []}
+        ]}
+    ]),
+    Port = 18080,
+    {ok, _} = cowboy:start_http(http_listener, 100,
+        [{port, Port}],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
+    game_sup:start_link().
+%%+++++++++++++++++++++++++++++++++++++++++++++++++
+
+stop(_State) ->
+    ok.
+%%+++++++++++++++++++++++++++++++++++++++++++++++++
