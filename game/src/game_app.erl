3bb9f5da56046c602f58d0c34904e59a32cd8bfc
Dmitry Davidenko <system.out@yandex.ru>
2016-12-04 11:20:20 +0700

    Добавил работу со статикой

--- game/src/game_app.erl
+++ game/src/game_app.erl
@@ -3,25 +3,44 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([
+  start/2,
+  stop/1,
+  dispatch_rules/0
+]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
+static_content_rule(Filetype) ->
+  {lists:append(["/", Filetype, "/[...]"]), cowboy_static,
+    {priv_dir, game, [list_to_binary(Filetype)], [
+      {mimetypes, cow_mimetypes, all}
+    ]}
+  }.
+%%+++++++++++++++++++++++++++++++++++++++++++++++++
+
+dispatch_rules() ->
+  cowboy_router:compile([
+    {'_', [
+      static_content_rule("css"),
+      static_content_rule("js"),
+      static_content_rule("img"),
+      {"/", index_handler, []},
+      {'_', notfound_handler, []}
+    ]}
+  ]).
+%%+++++++++++++++++++++++++++++++++++++++++++++++++
+
 start(_StartType, _StartArgs) ->
-    Dispatch = cowboy_router:compile([
-        {'_', [
-            {"/", index_handler, #{}},
-            {'_', notfound_handler, #{}}
-        ]}
-    ]),
-    Port = 18080,
-    {ok, _} = cowboy:start_clear(http_listener, 100,
-        [{port, Port}],
-        #{env => #{dispatch => Dispatch}}
-    ),
-    game_sup:start_link().
+  Dispatch = dispatch_rules(),
+  Port = 18080,
+  {ok, _} = cowboy:start_clear(http_listener, 100,
+    [{port, Port}],
+    #{env => #{dispatch => Dispatch}}
+  ),
+  game_sup:start_link().
 %%+++++++++++++++++++++++++++++++++++++++++++++++++
 
 stop(_State) ->
--- game/src/game_webserver.erl
+++ game/src/game_webserver.erl
@@ -3,6 +3,7 @@
 %% API
 -export([
     start/0,
+    update_routes/0,
     stop/0
 ]).
 
@@ -15,10 +16,17 @@
 start() ->
     ok = ensure_started(?APPS),
     ok = sync:go().
+%%+++++++++++++++++++++++++++++++++++++++++++++++++
+
+update_routes() ->
+    Routes = game_app:dispatch_rules(),
+    cowboy:set_env(http_listener, dispatch, Routes).
+%%+++++++++++++++++++++++++++++++++++++++++++++++++
 
 stop() ->
     sync:stop(),
     ok = stop_apps(lists:reverse(?APPS)).
+%%+++++++++++++++++++++++++++++++++++++++++++++++++
 
 %% ===================================================================
 %% Internal functions
@@ -30,8 +38,10 @@ ensure_started([App | Apps]) ->
         ok -> ensure_started(Apps);
         {error, {already_started, App}} -> ensure_started(Apps)
     end.
+%%+++++++++++++++++++++++++++++++++++++++++++++++++
 
 stop_apps([]) -> ok;
 stop_apps([App | Apps]) ->
     application:stop(App),
     stop_apps(Apps).
+%%+++++++++++++++++++++++++++++++++++++++++++++++++
